c4b45633b5a218ae362a466d762e4cf527c1ab24
Sumeet Jain <sumeetjain@berkeley.edu>
2014-11-02 00:06:59 -0700

    Changed sensitivty to coefficents

--- contacts/management/commands/createsettings.py
+++ contacts/management/commands/createsettings.py
@@ -5,9 +5,10 @@ class Command(BaseCommand):
     help = 'Creates three initial sound settings'
 
     def handle(self, *args, **options):
-        Settings.objects.create(name="Sensitive", sound_level=50, is_active=True)
+        Settings.objects.all().delete()
+        Settings.objects.create(name="Sensitive", sound_level=1.05, is_active=True)
         self.stdout.write('Created Sensitive Setting')
-        Settings.objects.create(name="Sorta Sensitive", sound_level=100, is_active=True)
+        Settings.objects.create(name="Sorta Sensitive", sound_level=1.5, is_active=True)
         self.stdout.write('Created Middle Sensitive Setting')
-        Settings.objects.create(name="Not Sensitive", sound_level=500, is_active=True)
+        Settings.objects.create(name="Not Sensitive", sound_level=2.0, is_active=True)
         self.stdout.write('Created Not Sensitive Setting')
