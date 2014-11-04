9edcd350f652c51bbaf59fde16ff26f9ee236ac9
Sumeet Jain <sumeetjain@berkeley.edu>
2014-11-04 00:48:26 -0800

    Changed default settings names

--- contacts/management/commands/createsettings.py
+++ contacts/management/commands/createsettings.py
@@ -6,9 +6,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Settings.objects.all().delete()
-        Settings.objects.create(name="Sensitive", sound_level=1.05, is_active=True)
+        sensitive_name = "Sensitive -- monitors sound near ambient threshold. Use for quiet study sessions."
+        sorta_name = "Sort of Sensitive -- monitors sound about 1.5 times the ambient threshold. Use for medium situations."
+        not_name = "Insensitive -- monitors sound about 2.0 times the ambient threshold. Use for loud situations."
+        Settings.objects.create(name=sensitive_name, sound_level=1.05, is_active=True)
         self.stdout.write('Created Sensitive Setting')
-        Settings.objects.create(name="Sorta Sensitive", sound_level=1.5, is_active=False)
+        Settings.objects.create(name=sorta_name, sound_level=1.5, is_active=False)
         self.stdout.write('Created Middle Sensitive Setting')
-        Settings.objects.create(name="Not Sensitive", sound_level=2.0, is_active=False)
+        Settings.objects.create(name=not_name, sound_level=2.0, is_active=False)
         self.stdout.write('Created Not Sensitive Setting')
